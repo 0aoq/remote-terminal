15a36b35b275464a1781b95326db61fb44d4ef34
0aoq <a0oq@outlook.com>
2022-09-05 17:13:50 -0400

    Better browser

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,5 +1,15 @@
-const { app, BrowserWindow, session } = require("electron");
-const cm = require("electron-context-menu");
+const {
+    app,
+    session,
+    protocol,
+
+    BrowserWindow,
+
+    Menu,
+    MenuItem,
+
+    ipcMain,
+} = require("electron");
 const { exec } = require("child_process");
 const path = require("node:path");
 const fs = require("node:fs");
@@ -12,11 +22,6 @@ let childprocess;
 let win = null;
 function createWindow() {
     // create window
-    cm({
-        showSearchWithGoogle: false,
-        showInspectElement: false,
-    });
-
     const window = new BrowserWindow({
         width: 667, // 80 columns
         height: 457, // 25 rows
@@ -27,7 +32,174 @@ function createWindow() {
         },
     });
 
-    win = window;
+    win = window; // doing this will force browser autocomplete for window.
+
+    // remote terminal transport protocol
+    protocol.registerHttpProtocol("rts", (request, callback) => {
+        let httpProtocol = "http";
+        let rtsProtocol = "rts";
+
+        const url = request.url.split(`${rtsProtocol}//`)[1];
+
+        request.url = `${httpProtocol}://${url}`;
+        request.session = null;
+
+        callback(request);
+    });
+
+    protocol.registerHttpProtocol("rtss", (request, callback) => {
+        let httpProtocol = "https";
+        let rtsProtocol = "rtss";
+        
+        const url = request.url.split(`${rtsProtocol}//`)[1];
+
+        request.url = `${httpProtocol}://${url}`;
+        request.session = null;
+
+        callback(request);
+    });
+
+    // create menu
+    const menu = [
+        { role: "fileMenu" },
+        { role: "editMenu" },
+        {
+            label: "Window",
+            submenu: [
+                { role: "minimize" },
+                { role: "zoom" },
+                { role: "reload" },
+                { role: "forceReload" },
+            ],
+        },
+        {
+            label: "View",
+            submenu: [
+                { role: "toggleDevTools" },
+                { type: "separator" },
+                { role: "resetZoom" },
+                { role: "zoomIn" },
+                { role: "zoomOut" },
+                { type: "separator" },
+                { role: "togglefullscreen" },
+            ],
+        },
+        {
+            label: "Browser",
+            submenu: [
+                {
+                    label: "Close Tab",
+                    click: async () => {
+                        window.webContents
+                            .executeJavaScript(`window.close();`)
+                            .catch(console.error);
+                    },
+                },
+            ],
+        },
+    ];
+    Menu.setApplicationMenu(Menu.buildFromTemplate(menu));
+
+    // context menu
+    function buildContextMenu(name, makeMenu) {
+        ipcMain.on(name, (event) => {
+            let _menu = makeMenu(event); // give the event to the builder
+            Menu.buildFromTemplate([..._menu, ...menu]).popup(
+                BrowserWindow.fromWebContents(event.sender)
+            );
+        });
+    }
+
+    // context menu text
+    buildContextMenu("show-context-menu-text", (event) => {
+        return [
+            // context menu for text
+            new MenuItem({ role: "cut" }),
+            new MenuItem({ role: "copy" }),
+            new MenuItem({ role: "paste" }),
+            new MenuItem({ role: "selectAll" }),
+            new MenuItem({ type: "separator" }),
+        ];
+    });
+
+    // context menu images
+    buildContextMenu("show-context-menu-image", (event) => {
+        return [
+            // context menu for text
+            new MenuItem({
+                label: "Copy Image Address",
+                click: async () => {
+                    event.sender.send("context-command", "copy-img-addr");
+                },
+            }),
+
+            new MenuItem({
+                label: "Open Image in New Tab",
+                click: async () => {
+                    event.sender.send("context-command", "open-img-addr");
+                },
+            }),
+
+            new MenuItem({ type: "separator" }),
+        ];
+    });
+
+    // base context menu
+    buildContextMenu("show-context-menu", (event) => {
+        return [
+            new MenuItem({
+                label: "Navigate",
+                click: async () => {
+                    event.sender.send("context-command", "navigate-prompt"); // prompt the user for a new page url
+                },
+            }),
+
+            new MenuItem({ type: "separator" }),
+        ];
+    });
+
+    // handle window opening
+    const windowOpenHandler = ({ url }) => {
+        console.log(
+            `\x1b[1m\x1b[35m⬢ ~ \x1b[1m\x1b[32m Opening custom window! ${url}\x1b[0m`
+        );
+
+        const windowb = new BrowserWindow({
+            nodeIntegration: false,
+            parent: null,
+            width: 1000,
+            height: 800,
+            webPreferences: {
+                preload: path.join(__dirname, "preload.cjs"),
+                // devTools: false,
+                sandbox: true,
+                autoplayPolicy: "user-gesture-required",
+                plugins: true,
+            },
+        });
+
+        windowb.loadURL(url);
+        windowb.webContents.setWindowOpenHandler(windowOpenHandler);
+
+        console.log(
+            [
+                " ┌ \x1b[1mWindow Info\x1b[0m ─────────────────────────────────────────────────────────┐",
+                "",
+                `    \x1b[1m\x1b[34mProcess ID \x1b[0m- ${windowb.webContents.getProcessId()}`,
+                `    \x1b[1m\x1b[32mTitle \x1b[0m- ${windowb.webContents.getTitle()}`,
+                `    \x1b[1m\x1b[33mUserAgent \x1b[0m- ${windowb.webContents.getUserAgent()}`,
+                `    \x1b[1m\x1b[35mType \x1b[0m- ${windowb.webContents.getType()}`,
+                "",
+                " └──────────────────────────────────────────────────────────────────────┘",
+            ].join("\n\r")
+        );
+
+        return {
+            action: "deny", // we're denying this because we'll just open a new browserview
+        };
+    };
+
+    window.webContents.setWindowOpenHandler(windowOpenHandler);
 
     // start backend
     childprocess = exec(
@@ -57,7 +229,8 @@ function createWindow() {
             `\x1b[1m\x1b[35m⬢ ~ \x1b[1m\x1b[32m Attempting to load extension! ${dir}\x1b[0m`
         );
 
-        session.defaultSession.loadExtension(path.resolve(__dirname, "extensions", dir))
+        session.defaultSession
+            .loadExtension(path.resolve(__dirname, "extensions", dir))
             .then(({ id }) => {
                 console.log(
                     `\x1b[1m\x1b[35m⬢ ~ \x1b[1m\x1b[32m Extension loaded! ${id}\x1b[0m`
--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,2 +1,61 @@
-window.addEventListener("DOMContentLoaded", () => {
-});
+const { ipcRenderer } = require("electron");
+
+onload = () => {
+    let info = {
+        selectedImage: undefined,
+    };
+
+    window.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+
+        // send specific content menus
+        if (window.getSelection().toString() !== "") {
+            // we have selected text! send for text menu
+            ipcRenderer.send("show-context-menu-text");
+        } else if (e.target.nodeName === "IMG") {
+            // image selected
+            ipcRenderer.send("show-context-menu-image");
+
+            info.selectedImage = {
+                origin: window.location.origin,
+                src: e.target.getAttribute("src"),
+                location: e.target
+                    .getAttribute("src")
+                    .startsWith(window.location.origin)
+                    ? e.target.getAttribute("src")
+                    : `${window.location.href}/${e.target.getAttribute("src")}`,
+            };
+        } else {
+            // nothing special, send normal meny
+            ipcRenderer.send("show-context-menu");
+        }
+    });
+
+    ipcRenderer.on("context-command", (e, command) => {
+        switch (command) {
+            // images
+            case "copy-img-addr":
+                if (info.selectedImage !== undefined) {
+                    navigator.clipboard.writeText(info.selectedImage.location);
+                }
+
+                break;
+
+            case "open-img-addr":
+                if (info.selectedImage !== undefined) {
+                    window.open(info.selectedImage.location);
+                }
+
+                break;
+
+            // navigation
+            case "navigate-prompt":
+                window.location.href = "http://localhost:3057/browser.html?replace";
+                break;
+
+            // default
+            default:
+                break;
+        }
+    });
+};
